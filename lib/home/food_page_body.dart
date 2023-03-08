a3114126e3378bd669bb78349461c416fbe19c6f
Md. Shadiuzzaman Khan <68236926+Md-Shadiuzzaman-Khan@users.noreply.github.com>
2023-03-09 03:16:14 +0600

    Initial commit
    e-commerce app

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -89,12 +89,13 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               itemCount: 10,
               itemBuilder: (context, index){
               return Container(
-                margin: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
+                margin: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20, bottom: Dimensions.height10),
                 child: Row(
                   children: [
+                    //image container
                     Container(
-                      width:120,
-                      height: 120,
+                      width:Dimensions.listViewImgSize,
+                      height: Dimensions.listViewImgSize,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(Dimensions.radius20),
                         color: Colors.white38,
@@ -102,6 +103,39 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                           fit: BoxFit.cover,
                           image: AssetImage(
                             "assets/image/image1.jpeg"
+                          ),
+                        ),
+                      ),
+                    ),
+                    //text container
+                    Expanded(
+                      child: Container(
+                        height: Dimensions.listViewTextContSize,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(topRight: Radius.circular(Dimensions.radius20), bottomRight: Radius.circular(Dimensions.radius20),),
+                          color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: Dimensions.width10, right: Dimensions.width10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Bigtext(text: "Nutritions fruit meal in Dhaka"),
+                              SizedBox(height: Dimensions.height10,),
+                              SmallText(text: "With dhakaian characteristics"),
+                              SizedBox(height: Dimensions.height10,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconAndTextWidget(icon: Icons.circle_sharp, text: "Normal", iconColor: AppColors.iconColor1),
+
+                                  IconAndTextWidget(icon: Icons.location_on, text: "1.7km", iconColor: AppColors.mainColor),
+
+                                  IconAndTextWidget(icon: Icons.access_time_rounded, text: "32min", iconColor: AppColors.iconColor2),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
                       ),
